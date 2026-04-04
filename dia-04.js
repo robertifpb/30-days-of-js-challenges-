238de475f569dead383e12f0bc45fd73b6c1a8d0
Alex Robert <alex.braz@academico.ifpb.edu.br>
2026-04-03 22:55:28 -0300

    feat: adiciona função para calcular o fatorial de um número

---
+++ dia-04.js
@@ -0,0 +1,13 @@
+// Write a function to calculate the factorial of a given number.
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5)); // 120
+
+
